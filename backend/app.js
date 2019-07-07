10456f1f06607a937f8692ac7fea25dd3669c113
nkwochidubem <nkwochidubem@yahoo.com>
2019-07-07 07:42:06 +0100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-let express = require('express'),
+  let express = require('express'),
   path = require('path'),
   mongoose = require('mongoose'),
   cors = require('cors'),
@@ -10,15 +10,15 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dataBaseConfig.db, {
   useNewUrlParser: true
 }).then(() => {
-    console.log('Database connected sucessfully ')
+    console.log('Database connected sucessfully ');
   },
   error => {
-    console.log('Could not connected to database : ' + error)
+    console.log('Could not connected to database : ' + error);
   }
-)
+);
 
 // Set up express js port
-const studentRoute = require('./routes/student.route')
+const studentRoute = require('./routes/student.route');
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,23 +28,23 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 // Setting up static directory
-app.use(express.static(path.join(__dirname, 'dist/angular8-meanstack-angular-material')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 // RESTful API root
-app.use('/api', studentRoute)
+app.use('/api', studentRoute);
 
 // PORT
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
+  console.log('Connected to port ' + port);
+});
 
 // Find 404 and hand over to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+// app.use((req, res, next) => {
+  // next(createError(404));
+// });
 
 // Index Route
 app.get('/', (req, res) => {
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/angular8-meanstack-angular-material/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // error handler
